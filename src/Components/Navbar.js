59d0af923ac6145a8a488b5fa54ba018557abbbb
safeerahmed100 <safeerahmed23901@gmail.com>
2023-09-11 12:08:19 +0500

    Nav

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -34,7 +34,7 @@ function handleMenu(){
      
       <div className='NavLinks'>
         <NavLink to='/about' className='link' end>About</NavLink>
-        <NavLink to='/service' className='link serviceDrop' onMouseEnter={()=>setIsHover(true)} onMouseLeave={()=>setIsHover(false)}>IT Services & Solutions</NavLink>
+        <NavLink to='/service' className='link serviceDrop' onMouseEnter={()=>setIsHover(true)} onMouseLeave={()=>setIsHover(false)}>IT Services & Solutions<i class="fa-solid fa-caret-down"></i></NavLink>
         <div className={`dropDown ${isShow && 'dropDown-scroll'}`}>
           <div className='ServicesLink'>
           <span>IT Services And Solutions</span>
@@ -71,7 +71,7 @@ function handleMenu(){
         </div>
         <NavLink to='./it-staffing-services' className='link'>IT Staffing Services</NavLink> 
         <NavLink to='/industries'className='link' >Industries</NavLink>
-        <NavLink to='/locations'className='link locationDrop' >Locations</NavLink>
+        <NavLink to='/locations'className='link locationDrop' >Locations <i class="fa-solid fa-caret-down"></i></NavLink>
         <div className={`LocationDropDown ${isShow && 'LocationDropDown-scroll'}`}>
         <NavLink to='/it-managed-napearville' className='link' >Napearville</NavLink>
         <NavLink to='/it-managed-chicago' className='link' >Chicago</NavLink>
@@ -110,7 +110,39 @@ function handleMenu(){
          
       <div className='NavigationLinks'>
         <NavLink to='/about' className='link' end>About</NavLink>
-        <NavLink to='/service' className='link' >IT Services & Solutions</NavLink>
+        <NavLink to='/service' className='link' >IT Services & Solutions <i class="fa-solid fa-caret-down"></i></NavLink>
+        <div className='ServicesLink'>
+          <span><i class="fa-solid fa-caret-down"></i>IT Services And Solutions</span>
+          <div className='ServiceLinkTab'>
+        <NavLink to='./it-managed-services' className='link'>IT Managed Services</NavLink> 
+        <NavLink to='./it-consultancy-services' className='link'>IT Consulting Services</NavLink> 
+        <NavLink to='./it-assessment-audit-services' className='link'>IT Assessments & Audits</NavLink> 
+        <NavLink to='./it-deployement-services' className='link'>IT Deployement</NavLink> 
+        <NavLink to='./it-emergency-support' className='link'>IT Emergency Support</NavLink> 
+        <NavLink to='./it-help-desk' className='link'>IT Help Desk</NavLink> 
+        <NavLink to='./it-security-services' className='link'>IT Security Services</NavLink> 
+        <NavLink to='./office-365' className='link'>Office 365</NavLink> 
+        <NavLink to='./low-voltage-cabling' className='link'>IT Low Voltage Cabling</NavLink> 
+        <NavLink to='./email-spam-protection' className='link'>Email & Spam Protection</NavLink> 
+        <NavLink to='./security-systems' className='link'>Security Systems</NavLink> 
+        </div>
+        </div>
+        <div className='CompetenciesLink'>
+          <span><i class="fa-solid fa-caret-down"></i>IT Competencies </span>
+          <div className='CompetenciesLinkTab'>
+          <NavLink to='./it-competencies' className='link'>IT Competencies</NavLink>
+          <NavLink to='./net-framework'  className='link'>Net Framework</NavLink> 
+          <NavLink to='./active-directory'  className='link'>Active Directory</NavLink> 
+          <NavLink to='./exchange'   className='link'>Exchange Directory</NavLink> 
+          <NavLink to='./lan-wan'   className='link'>LAN / WAN</NavLink> 
+          <NavLink to='./mobility'   className='link'>Mobility</NavLink> 
+          <NavLink to='./sharepoint'   className='link'>Sharepoint</NavLink> 
+          <NavLink to='./virtualization'   className='link'>Virtualization</NavLink> 
+          <NavLink to='./sql-server'   className='link'>Sql Server</NavLink> 
+          <NavLink to='./vpn'   className='link'>VPN</NavLink> 
+          <NavLink to='./industries'   className='link'>Industries</NavLink> 
+        </div>
+        </div>
         <NavLink to='/networksecurity' className='link'>Network Security</NavLink>
         <NavLink to='/industries'className='link' >Industries</NavLink>
         <NavLink to='/locations'className='link' >Locations</NavLink>
