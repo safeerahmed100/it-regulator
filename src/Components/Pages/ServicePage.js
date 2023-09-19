e067a72b0243274f5747f78b0429d0eb8c4c9576
Safeer Ahmed <SafeerAhmed@gmail.com>
2023-09-19 12:45:14 -0700

    checking

--- src/Components/Pages/ServicePage.js
+++ src/Components/Pages/ServicePage.js
@@ -8,7 +8,7 @@ function ServicePage() {
 <>    <ServicesHeader/>
     <div className='ServiceSlider'>
 
-    <h1><span style={{borderBottom:'1px solid red'}}>Services</span> offered by ITRegulators include:</h1>
+    <h1><span style={{borderBottom:'1px solid red'}}>Checking if working</span> offered by ITRegulators include:</h1>
     
     <ServicesSlider componentClass='ServicePageSlider'/>
     <ServicesSlider componentClass='ServicePageSlider'/>
