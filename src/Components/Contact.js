ad6ed0121c37aeb4aac71717aba645d057c2443e
safeerahmed100 <safeerahmed23901@gmail.com>
2023-09-19 17:35:01 +0500

    Form Validation

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,45 +1,66 @@
-import React from 'react'
-import './css/Contact.css'
-// import ContactPopUp from './ContactPopUp'
-import {Fade, Slide} from 'react-awesome-reveal'
+import React, { useState } from 'react';
+import './css/Contact.css';
+import { Fade, Slide } from 'react-awesome-reveal';
+import { useForm, ValidationError } from '@formspree/react';
 
-function Contact({componentClass}) {
+function Contact({ componentClass }) {
+  const [state, handleSubmit] = useForm("xpzgjpen");
+
+  if (state.succeeded) {
+    return (
+      <div className='AfterFormSubmit'>
+        <i className='fa-solid fa-message'></i>
+        <h4>Thanks for Submitting</h4>
+        <p>Your message has been sent!</p>
+        <button>Go Back</button>
+      </div>
+    );
+  }
+
   return (
     <div className={`Contact ${componentClass}`}>
       <div className='ContactContainer'>
-        <Slide >   
-             <div className='ContactText'>
-
-      <h1>NEED IT <span style={{color:'#ff3939'}}>HELP</span> NOW?</h1>
-      <h3>Fill in the the form and we’ll get back to you as soon as possible</h3>
-      </div>
-      <div className='ContactForm'>
-        <Fade cascade damping={0.5}>
-        <form >
-        <select placeholder='Service'>
-          <option>Services</option>
-          <option>Web Development</option>
-          <option>App Development</option>
-          <option>SEO Development</option>
-        </select>
-        <input  className='inputName' type='name' placeholder='Full Name'/>
-        <input className='inputEmail' type='email' placeholder='Email' />
-        <input className='inputPhone' type='phone' placeholder='Phone' />
-        <input className='inputBusiness' type='text' placeholder='Business Name'/>
-        <input  className='inputMessage' type='text' placeholder='Your Message' />
-        <button className='btn'>Send</button>
-        </form>
-       
-
+        <Slide>
+          <div className='ContactText'>
+            <h1>
+              NEED IT <span style={{ color: '#ff3939' }}>HELP</span> NOW?
+            </h1>
+            <h3>Fill in the form and we’ll get back to you as soon as possible</h3>
+          </div>
+          <div className='ContactForm'>
+            <Fade cascade damping={0.5}>
+              <form onSubmit={handleSubmit}>
+                <select id='Service' name='Selected Service' placeholder='Service' required>
+                  <option value=''>Services</option>
+                  <option value='Web Development'>Web Development</option>
+                  <option value='App Development'>App Development</option>
+                  <option value='SEO Development'>SEO Development</option>
+                </select>
+                <input className='inputName' name='Name' type='text' placeholder='Full Name' required />
+                <ValidationError prefix='Name' field='Name' errors={state.errors} />
+                <input className='inputEmail' name='Email' type='email' placeholder='Email' required />
+                <ValidationError prefix='Email' field='Email' errors={state.errors} />
+                <input className='inputPhone' name='Phone' type='tel' placeholder='Phone' required />
+                <ValidationError prefix='Phone' field='Phone' errors={state.errors} />
+                <input className='inputBusiness' type='text' name='Business' placeholder='Business Name' />
+                <textarea
+                  rows='5'
+                  className='inputMessage'
+                  name='message'
+                  placeholder='Your Message'
+                  required
+                />
+                <ValidationError prefix='Message' field='message' errors={state.errors} />
+                <button className='btn' type='submit'>
+                  Send
+                </button>
+              </form>
             </Fade>
+          </div>
+        </Slide>
       </div>
-      </Slide>
-
-      </div>
-  
     </div>
-
-  )
+  );
 }
 
-export default Contact
+export default Contact;
